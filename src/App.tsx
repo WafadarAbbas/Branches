2040bc03033272ec3cc0b840cbd36691fcad1b15
Wafadar Abbas <168413968+WafadarAbbas@users.noreply.github.com>
2026-04-02 13:59:40 +0500

    Add TestScreen for F-Test feature

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,7 @@
-import { useState } from 'react'
-import './App.css'
+import TestScreen from "./Pages/TestScreen";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div style={{ textAlign: "center", marginTop: "50px" }}>
-        <h1>🚀 We are working on Git branches</h1>
-        <p>This is a feature branch development example</p>
-
-        <button onClick={() => setCount(count + 1)}>
-          Coun: {count}
-        </button>
-      </div>
-    </>
-  )
+  return <TestScreen />;
 }
 
-export default App
+export default App;
