8322e10eedd13d810cbd666141268df977997c0b
Wafadar Abbas <168413968+WafadarAbbas@users.noreply.github.com>
2026-04-02 14:13:51 +0500

    Revert "Add TestScreen for F-Test-feature"
    This reverts commit 2040bc03033272ec3cc0b840cbd36691fcad1b15.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
-import TestScreen from "./Pages/TestScreen";
+import { useState } from 'react'
+import './App.css'
 
 function App() {
-  return <TestScreen />;
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div style={{ textAlign: "center", marginTop: "50px" }}>
+        <h1>🚀 We are working on Git branches</h1>
+        <p>This is a feature branch development example</p>
+
+        <button onClick={() => setCount(count + 1)}>
+          Coun: {count}
+        </button>
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
